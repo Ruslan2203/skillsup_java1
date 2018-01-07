0b79dbd0b7b03c6574b46f052303d759907ba054
ruslan2203 <ruslan0505@gmail.com>
2018-01-07 15:01:05 +0200

    Create annotation @Execute targeted for methods.

--- src/main/java/ua/dp/skillsup/reflection/Execute.java
+++ src/main/java/ua/dp/skillsup/reflection/Execute.java
@@ -1,6 +1,8 @@
 package ua.dp.skillsup.reflection;
 import java.lang.annotation.*;
-@Target(value=ElementType.METHOD)
 
+@Target(value=ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Execute {
+
 }
